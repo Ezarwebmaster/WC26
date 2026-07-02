0b2fa05129eb670c39eacdbd833da16c08a2a194
Ezarwebmaster <Ezar.webmaster@GMAIL.COM>
2026-07-02 23:27:05 +0100

    feat: add support for Google Analytics via environment variable

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Dynamically inject Google Analytics if configured
+const gaId = import.meta.env.VITE_GA_ID;
+if (gaId && typeof window !== "undefined") {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script);
+
+  const inlineScript = document.createElement("script");
+  inlineScript.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaId}', { page_path: window.location.pathname });
+  `;
+  document.head.appendChild(inlineScript);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
